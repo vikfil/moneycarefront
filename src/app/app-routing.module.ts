b6bc9d1e8303e1f4ee2e6a5efda3b54011976c7d
Viktor Fylypchuk <vikfil87@gmail.com>
2021-07-01 11:47:36 +0300

    "initial commit"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ConfirmComponent } from './component/confir-account/confirm-component';
+import { HeaderComponent } from './component/header/header-component';
+import { HomeComponent } from './component/home-component';
+import { LoginComponent } from './component/login/login-component';
+import {RegisterComponent} from './component/registration/registration-component';
+import { AuthGuardService } from './service/auth-guard-service';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'confirm', component: ConfirmComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
+  {path: '**', redirectTo: '/'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserService } from './service/user-service';
+import { AuthenticationService } from './service/authenticated-service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { LoginComponent } from './component/login/login-component';
+import { RegisterComponent } from './component/registration/registration-component';
+import { ConfirmComponent } from './component/confir-account/confirm-component';
+import { HeaderComponent } from './component/header/header-component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { IncomeService } from './service/income-service';
+import { HomeComponent } from './component/home-component';
+import { AuthInterceptor } from './service/auth-interceptor';
+import { BasicInterceptorService } from './service/basic-interceptor-service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfirmComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // ToastrModule.forRoot({
+    //   timeOut: 10000,
+    //   positionClass: 'toast-top-center',
+    //   enableHtml: true
+    // }),
   ],
-  providers: [],
+  providers: [
+   // UserService,
+    {provide: HTTP_INTERCEPTORS, useClass: BasicInterceptorService, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    //  IncomeService, 
+    //  AuthenticationService
+    // ToastrService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
